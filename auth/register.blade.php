26d43787618591ef51b98f315f8ed7405c4f9783
galennolan <galen.nolan1@gmail.com>
2023-03-08 12:34:14 +0700

    first commit

---
+++ auth/register.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initialscale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Pemrograman Akuntansi I</title>
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('asset/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('asset/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    
+</head>
+<body class="bg-gradient-light">
+    <div class="text-center">
+          <br><img src="{{ asset('asset/img/logo_ubsi.png')}}" width="100"></h1>
+          
+         </div>
+     <div class="container">
+         <!-- Outer Row -->
+         
+        <div class="row justify-content-center">
+         <div class="col-xl-5 col-lg-12 col-md-9">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                        <div class="center">
+                        <div class="col-sm">
+                        <div class="p-5">
+           
+                        <form method="POST" action="{{ route('register') }}">
+                         @csrf
+                        <div class="form-group row">
+                        <h5 center>    Daftar  </h5>
+                            <div class="col-md-12">
+                                <input id="name" required placeholder="nama" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    
+                        <div class="form-group row">
+                            <div class="col-md-12">
+                                <input id="email" required placeholder="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+    
+                        <div class="form-group row">
+                          
+
+                            <div class="col-md-12">
+                                <input id="password" required placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                          
+
+                            <div class="col-md-12">
+                                <input id="password-confirm" required placeholder="Password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-12 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('   Daftar  ') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+            <!-- Bootstrap core JavaScript-->
+            <script src="{{ asset('asset/vendor/jquery/jquery.min.js')}}"></script>
+            <script src="{{ asset('asset/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+            <!-- Core plugin JavaScript-->
+            <script src="{{ asset('asset/vendor/jqueryeasing/jquery.easing.min.js')}}"></script>
+            <!-- Custom scripts for all pages-->
+            <script src="{{ asset('asset/js/sb-admin-2.min.js')}}"></script>
+    </body>
+</html>
